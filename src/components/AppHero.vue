149f8f71d641db7723dce5ba5429b23c9a9a7a26
EdoardoPerucca <edoardo.perucca@hotmail.com>
2023-03-23 18:40:51 +0100

    exercise completed

---
+++ src/components/AppHero.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+};
+</script>
+
+<template>
+    <div>
+        <img src="/jumbooo.jpg" alt="jumbo">;
+    </div>
+</template>
+
+<style>
+img {
+    max-width: 700px;
+    height: auto;
+}
+</style>
